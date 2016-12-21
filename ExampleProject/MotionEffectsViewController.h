01cd22463851315426df2f1fde530f25b76311e1
Markus Emrich <markus.emrich@gmail.com>
2016-12-20 18:19:38 -0800

    Implement MotionEffectsViewController in code to make use of the lay outing helpers

--- ExampleProject/MotionEffectsViewController.h
+++ ExampleProject/MotionEffectsViewController.h
@@ -7,7 +7,4 @@
 
 @interface MotionEffectsViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
-@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *labels;
-
 @end
--- ExampleProject/MotionEffectsViewController.m
+++ ExampleProject/MotionEffectsViewController.m
@@ -6,8 +6,60 @@
 #import "UIView+SimpleMotionEffects.h"
 
 #import "MotionEffectsViewController.h"
+#import "UIView+position.h"
 
 @implementation MotionEffectsViewController
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+
+  self.view.clipsToBounds = YES;
+
+  // background image
+  UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"wallpaper"]];
+  imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+  imageView.frame = CGRectMake(0, 0, 900, 900);
+  imageView.frameOrigin = CGPointMake(-250, -100);
+  [imageView addMotionEffectWithMovement:CGPointMake(100, 100)];
+  [self.view addSubview:imageView];
+
+  UIView *dimmingView = [[UIView alloc] initWithFrame:self.view.bounds];
+  dimmingView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+  dimmingView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.25];
+  [self.view addSubview:dimmingView];
+
+  // labels
+  const NSUInteger labelCount = 6;
+  const NSUInteger labelHeight = 30;
+  const NSUInteger labelMargin = 15;
+  const CGFloat startY = floor((self.view.frameHeight - ((labelHeight + labelMargin) * labelCount)) / 2.0);
+  const CGPoint baseStrenght = CGPointMake(self.view.frameWidth / (labelCount * 2),
+                                           self.view.frameHeight / (labelCount * 4));
+  for (NSInteger i=0; i<labelCount; i++) {
+    const CGFloat yPosition = (startY + (labelHeight+labelMargin) * i);
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, yPosition, self.view.frameWidth, labelHeight)];
+    label.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+    label.textAlignment = NSTextAlignmentCenter;
+    label.text = [NSString stringWithFormat:@"Label%02zd", i+1];
+    label.textColor = [UIColor whiteColor];
+    label.font = [UIFont systemFontOfSize:17 + 2*i];
+    [label addMotionEffectWithMovement:CGPointMake(baseStrenght.x * i,
+                                                   baseStrenght.y * i)];
+    [self.view addSubview:label];
+  }
+
+  // back button
+  UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+  button.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin;
+  [button setTitle:@"Back" forState:UIControlStateNormal];
+  button.tintColor = [UIColor whiteColor];
+  button.titleLabel.font = [UIFont systemFontOfSize:18];
+  [button sizeToFit];
+  button.frameOrigin = CGPointMake(20, 40);
+  [button addTarget:self action:@selector(goBack:) forControlEvents:UIControlEventTouchUpInside];
+  [self.view addSubview:button];
+}
 
 - (void)viewWillAppear:(BOOL)animated
 {
@@ -21,29 +73,16 @@
   [self.navigationController setNavigationBarHidden:NO animated:YES];
 }
 
-- (IBAction)goBack:(id)sender
-{
-  [self.navigationController popViewControllerAnimated:YES];
-}
-
-- (void)viewDidLoad
-{
-  [super viewDidLoad];
-
-  // labels
-  CGFloat strenghtX = self.view.frame.size.width / (self.labels.count * 2);
-  CGFloat strenghtY = self.view.frame.size.height / (self.labels.count * 4);
-  [self.labels enumerateObjectsUsingBlock:^(UILabel *label, NSUInteger idx, BOOL *stop) {
-    [label addMotionEffectWithMovement:CGPointMake(strenghtX*idx, strenghtY*idx)];
-  }];
-
-  // image
-  [self.imageView addMotionEffectWithMovement:CGPointMake(100, 100)];
-}
-
 - (UIStatusBarStyle)preferredStatusBarStyle
 {
   return UIStatusBarStyleLightContent;
+}
+
+#pragma mark - Actions
+
+- (void)goBack:(id)sender
+{
+  [self.navigationController popViewControllerAnimated:YES];
 }
 
 @end
